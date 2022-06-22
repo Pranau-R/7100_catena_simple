3d1becd55fdd95c7a2fe87307c2611daca5ee44a
PranauR <ajaypranauer10@gmail.com>
2022-06-22 19:00:27 +0530

    #1: updated with header

--- 7100_catena_uart/7100_catena_uart.ino
+++ 7100_catena_uart/7100_catena_uart.ino
@@ -1,7 +1,52 @@
+/*
+Module: 7100_catena_uart.ino
+
+Function:
+        Test sketch to connect MCCI Catena4610 board and Piera Particle Sensor-7100 using UART Protocol
+
+Copyright notice:
+        This file copyright (C) 2022 by
+        MCCI Corporation
+        3520 Krums Corners Road
+        Ithaca, NY 14850
+        An unpublished work. All rights reserved.
+        This file is proprietary information, and may not be disclosed or
+        copied without the prior permission of MCCI Corporation.
+
+Author:
+        Pranau R, MCCI Corporation   June 2022
+*/
+
 #include <Catena.h>
 
 String serial1Out;
 String serialIn;
+
+/****************************************************************************\
+|
+|   Code.
+|
+\****************************************************************************/
+
+/*
+
+Name:	setup()
+
+Function:
+        Arduino setup function.
+
+Definition:
+        void setup(
+            void
+            );
+
+Description:
+        To initiate a serial connection between board and display and to check the connectivity of 7100 Particle Sensor.
+
+Returns:
+        No explicit result.
+
+*/
 
 void setup() 
     {
@@ -11,10 +56,8 @@ void setup()
     while (! Serial)
     Serial1.begin(115200);
     Serial.println("");
-    Serial.println("****  7100 ESP32 Example ****");
-    Serial.println("****  Piera Systems      ****");
+    Serial.println("****  7100 Particle Sensor Example  ****");
     Serial.println("");
-    Serial.println("Warming up 7100...");
 
     // Wait 5 seconds for 7100 to boot up before sending commands
     delay(5000);
@@ -28,6 +71,21 @@ void setup()
     Serial1.write("$Wazure=0\r\n");
     delay(100);
     }
+
+/*
+Name:   loop()
+
+Function:
+        To get particle readings and display it in serial monitor.
+
+Definition:
+        void loop (
+            void
+            );
+
+Returns:
+        No explicit result.
+*/
 
 void loop()
     {
