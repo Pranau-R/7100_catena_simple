573de5b6109980c438bbc292f08b2d5beb3645be
PranauR <ajaypranauer10@gmail.com>
2022-06-22 18:59:37 +0530

    WIP: fixed begin issue

--- 7100_catena_i2c/7100_catena_i2c.ino
+++ 7100_catena_i2c/7100_catena_i2c.ino
@@ -1,3 +1,22 @@
+/*
+Module: 7100_catena_i2c.ino
+
+Function:
+        Test sketch to connect MCCI Catena4610 board and Piera Particle Sensor-7100 using I2C Protocol
+
+Copyright notice:
+        This file copyright (C) 2022 by
+        MCCI Corporation
+        3520 Krums Corners Road
+        Ithaca, NY 14850
+        An unpublished work. All rights reserved.
+        This file is proprietary information, and may not be disclosed or
+        copied without the prior permission of MCCI Corporation.
+
+Author:
+        Pranau R, MCCI Corporation   June 2022
+*/
+
 #include <Wire.h>
 #include <Catena.h>
 #include <IpsI2C.h>
@@ -10,21 +29,72 @@ cIpsSensor ips_sensor;
 //int fail_count;
 //int last = true;
 
+/****************************************************************************\
+|
+|   Code.
+|
+\****************************************************************************/
+
+/*
+
+Name:	setup()
+
+Function:
+        Arduino setup function.
+
+Definition:
+        void setup(
+            void
+            );
+
+Description:
+        To initiate a serial connection between board and display and to check the connectivity of 7100 Particle Sensor.
+
+Returns:
+        No explicit result.
+
+*/
+
 void setup()
     {
     // Initiate USB serial at 115200 baud
     Serial.begin(115200);
     while (! Serial);
-    Serial.println("\nI2C Test");
+    Serial.println("\n****  7100 Particle Sensor Example ****");
     // Wait on IPS boot
     delay(1000);
 
+    //ips_sensor.begin();
+
+    while(!ips_sensor.begin())
+        {
+        Serial.println("7100 Particle Sensor not connected!\n");
+        delay(5000);
+        }
+
+    Serial.println("7100 Particle Sensor connected successfully!\n");
+    delay (2000);
     // Initiate I2C connection SDA 21, SCL 22 by default
     // Set different pins by using:
     // ips_sensor.begin(SDA_PIN, SCL_PIN)
     //ips_sensor.begin(36, 35);;
     Wire.begin(I2C_ADDRESS);
     }
+
+/*
+Name:   loop()
+
+Function:
+        To get particle readings and display it in serial monitor.
+
+Definition:
+        void loop (
+            void
+            );
+
+Returns:
+        No explicit result.
+*/
 
 void loop()
     {
